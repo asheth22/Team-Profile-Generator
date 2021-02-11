bfaa7713cf299f226481d1c166d759a6730a0391
Anjali Sheth <anjali.sheth@otis.com>
2021-02-11 13:08:52 -0500

    added constructors

---
+++ data/employee.js
@@ -0,0 +1,23 @@
+// Constructor for Employee
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = "Employee";
+    };
+    getName() {
+        return this.name;
+    };
+    getId() {
+        return this.id;
+    };
+    getEmail() {
+        return this.email;
+    };
+    getRole() {
+        return this.role;
+    }
+}
+
+module.exports = Employee;
---
+++ data/engineer.js
@@ -0,0 +1,16 @@
+// Extends Employee class to add Engineer
+
+const Employee = require("./Employee.js");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.role = "Engineer";
+        this.github = github;
+    }
+    getGithub() {
+        return this.github;
+    };
+}
+
+module.exports = Engineer;
